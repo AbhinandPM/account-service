0e69362880dd9d745e94cdd98e8c5e05b5cd0551
Abhi <abhinandclt@gmail.com>
2020-09-17 10:22:25 +0530

    Code changes for adding exception handling for jms message sending

--- src/main/java/com/abhi/account/service/TransactionServiceImpl.java
+++ src/main/java/com/abhi/account/service/TransactionServiceImpl.java
@@ -3,6 +3,8 @@ package com.abhi.account.service;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.core.JmsTemplate;
@@ -23,6 +25,8 @@ import com.abhi.account.util.InvalidInputException;
 @Transactional
 public class TransactionServiceImpl implements TransactionService {
 
+	private static final Logger LOGGER = LogManager.getLogger(TransactionServiceImpl.class);
+
 	@Autowired
 	private TransactionRepository transactionRepo;
 
@@ -31,7 +35,7 @@ public class TransactionServiceImpl implements TransactionService {
 
 	@Autowired
 	private AccountRepository accountRepo;
-	
+
 	@Autowired
 	private JmsTemplate jmsTemplate;
 
@@ -51,7 +55,12 @@ public class TransactionServiceImpl implements TransactionService {
 			Transaction transaction = CustomBeanUtility.convertToDomain(transactionDto);
 			accountTransactionDao.performTransaction(transaction);
 			transactionDto = CustomBeanUtility.convertToDto(transactionRepo.save(transaction));
-			jmsTemplate.convertAndSend(transactionStatusQueue, transactionDto);
+			try {
+				jmsTemplate.convertAndSend(transactionStatusQueue, transactionDto);
+			} catch (Exception e) {
+				LOGGER.error(e.getMessage(), e);
+			}
+
 			return transactionDto;
 		} else {
 			throw new AccountDetailsNotFoundException();
